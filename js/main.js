d816eaba5879561d9b4989b7d7065a133263f5bb
zhixoingwang <1064517852@qq.com>
2018-04-11 00:16:09 +0800

    g

--- js/main.js
+++ js/main.js
@@ -1,22 +1,21 @@
-function nTabs(thisObj,Num){
+function nTabs(thisObj, Num) {
     console.log(11);
-    if(thisObj.className == "active")return;
+    if (thisObj.className == "active")return;
     var tabObj = thisObj.parentNode.id;
     var tabList = document.getElementById(tabObj).getElementsByTagName("li");
-    for(i=0; i <tabList.length; i++)
-    {
-        if (i == Num)
-        {
+    for (i = 0; i < tabList.length; i++) {
+        if (i == Num) {
             thisObj.className = "on";
-            document.getElementById(tabObj+"_Content"+i).style.display = "block";
-        }else{
+            document.getElementById(tabObj + "_Content" + i).style.display = "block";
+        } else {
             tabList[i].className = "";
-            document.getElementById(tabObj+"_Content"+i).style.display = "none";
+            document.getElementById(tabObj + "_Content" + i).style.display = "none";
         }
     }
 }
 // JavaScript Document
-$(function() {
+$(function () {
+    // 底部图片hover效果
     $(".weibo_icon").hover(function () {
         $(".weibo").fadeIn(500);
         $(".qq,.twitter,.wechat").fadeOut(0);
@@ -58,65 +57,114 @@ $(function() {
         $(".code_wrap").css("z-index", 50);
     });
 
+    var li = $('.nav ul .m');
+    li.eq(0).find('a').eq(0).hover(function () {
+        $(this).html('HOME');
+    }, function () {
+        $(this).html('首页');
+    });
+
+    li.eq(1).find('a').eq(0).hover(function () {
+        $(this).html('WEBSITE');
+    }, function () {
+        $(this).html('网站建设');
+    });
+
+    li.eq(2).find('a').eq(0).hover(function () {
+        $(this).html('PRODUCT');
+    }, function () {
+        $(this).html('产品服务');
+    });
+
+    li.eq(3).find('a').eq(0).hover(function () {
+        $(this).html('CASE');
+    }, function () {
+        $(this).html('成功案例');
+    });
+
+    li.eq(4).find('a').eq(0).hover(function () {
+        $(this).html('ABOUT');
+    }, function () {
+        $(this).html('关于我们');
+    });
+    var url = window.location.href.toLowerCase();
+    if (url.indexOf("/index.html") > -1) {
+        $(".aa1").attr("id", "sel");
+    } else if (url.indexOf("/news/") > -1 ) {
+        $(".aa5").attr("id", "sel");
+    }
+
+    //控制导航
+    var nav = $('.navbar'),
+        doc = $(document),
+        win = $(window);
+    win.scroll(function () {
+        if (doc.scrollTop() > 80) {
+            nav.addClass('scrolled');
+        } else {
+            nav.removeClass('scrolled');
+        }
+    });
+    win.scroll();
 
 
     $(".cont dl").hover(
         function () {
-            $(this).children().stop(false,true);
+            $(this).children().stop(false, true);
             $(this).children(".serBoxOn").fadeIn();
-            $(this).children(".pic1").animate({right: -205},400);
-            $(this).children(".pic2").animate({left: 28},400);
-            $(this).children(".txt1").animate({left: -261},400);
-            $(this).children(".txt2").animate({right: 0},400);
+            $(this).children(".pic1").animate({right: -205}, 400);
+            $(this).children(".pic2").animate({left: 28}, 400);
+            $(this).children(".txt1").animate({left: -261}, 400);
+            $(this).children(".txt2").animate({right: 0}, 400);
         },
         function () {
-            $(this).children().stop(false,true);
-            $(this).children(".serBoxOn").fadeOut( );
-            $(this).children(".pic1").animate({right:28},400);
-            $(this).children(".pic2").animate({left: -205},400);
-            $(this).children(".txt1").animate({left: 0},400);
-            $(this).children(".txt2").animate({right: -261},400);
+            $(this).children().stop(false, true);
+            $(this).children(".serBoxOn").fadeOut();
+            $(this).children(".pic1").animate({right: 28}, 400);
+            $(this).children(".pic2").animate({left: -205}, 400);
+            $(this).children(".txt1").animate({left: 0}, 400);
+            $(this).children(".txt2").animate({right: -261}, 400);
         }
     );
 
-    var newhtml, i,s;
+    var newhtml, i, s;
     i = 0;
     s = 0;
-    $(".nTab .cont ul").each(function() {
+    $(".nTab .cont ul").each(function () {
         var xs = parseInt($(this).children("li").css("margin-right").replace('px', ''));
 
         $(this).width(($(this).children("li").width() + xs) * $(this).children("li").length)
     });
     /*左移*/
 
-    function shangfan(){
-        var ulElement =  $(".nTab .cont ul");
+    function shangfan() {
+        var ulElement = $(".nTab .cont ul");
 
-        var speed =  1000;
-        ulElement.each(function(){
-            var lineHeight = -( $(this).children("li").width()+parseInt( $(this).children("li").css("margin-right").replace('px', '')));
+        var speed = 1000;
+        ulElement.each(function () {
+            var lineHeight = -( $(this).children("li").width() + parseInt($(this).children("li").css("margin-right").replace('px', '')));
 
             $(this).find("li:last").prependTo($(this));
-            $(this).css({marginLeft:lineHeight});
+            $(this).css({marginLeft: lineHeight});
             $(this).animate({
-                marginLeft:0
-            },speed,function(){
-                $(this).css({marginLeft:0});
+                marginLeft: 0
+            }, speed, function () {
+                $(this).css({marginLeft: 0});
 
             });
         })
     }
 
 
-    function xiafan(){
+    function xiafan() {
         var ulElement = $(".nTab .cont ul");
-        var speed =  1000;
-        ulElement.each(function(){
-            var lineHeight = ( $(this).children("li").width()+parseInt( $(this).children("li").css("margin-right").replace('px', '')));
+        var speed = 1000;
+        ulElement.each(function () {
+            var lineHeight = ( $(this).children("li").width() + parseInt($(this).children("li").css("margin-right").replace('px', '')));
             $(this).animate({
-                marginLeft:-lineHeight
-            },speed,function(){
-                $(this).css({marginLeft:0});
+                marginLeft: -lineHeight
+            }, speed, function () {
+                $(this).css({marginLeft: 0});
                 $(this).find("li:first").appendTo($(this));
 
             });
@@ -125,9 +173,12 @@ $(function() {
     }
 
 
-
-    $(".prev").click(function() {
-        console.log(1);shangfan(); });
-    $(".next").click(function() { xiafan(); })
+    $(".prev").click(function () {
+        console.log(1);
+        shangfan();
+    });
+    $(".next").click(function () {
+        xiafan();
+    })
 })
 
